f13fb8f91d49354032ca1177cfd3e9027c48b2da
Amecaba <kabaamekabaame@gmail.com>
2016-02-28 17:21:53 +0900

    20160228 YotaroIshizaki add advettisementspace

--- TestCalculater1/app/src/main/java/com/example/yotaro/testcalculater1/MainActivity.java
+++ TestCalculater1/app/src/main/java/com/example/yotaro/testcalculater1/MainActivity.java
@@ -7,12 +7,13 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class MainActivity extends ActionBarActivity
+public class MainActivity extends AppCompatActivity
 //        implements NavigationDrawerFragment.NavigationDrawerCallbacks
 {
 
